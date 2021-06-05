d7bcb7100280e4baaa460571fab95163b5c9ea60
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-06-04 23:37:06 -0700

    converted TodoForm class constructor and methods to fn component

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,4 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function TodoForm (props) {
+
+  const[item, setItem] = useState({});
+
+  const handleInputChange = e => {
+    setItem({ ...item, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    const reset = {};
+    setItem(reset);
+  }
+}
 
 class TodoForm extends React.Component {
 
