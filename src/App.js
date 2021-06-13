43d866a26d94d1938a028cf270b22b56b8c5113d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-06-12 23:38:07 -0700

    added some context features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SiteContext from './components/context/context.js';
 import ToDo from './components/todo/todo.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <ToDo />
+        <SiteContext>
+          <ToDo />
+        </SiteContext>
       </>
     );
   }
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { useState, useContext } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import { SiteContext } from '../context/context.js';
 
 function TodoList (props) {
+
+  const siteInformation = useContext(SiteContext);
+
+  const hideComplete = (e) => {
+    siteInformation.changeHideCompleteTo(true);
+  }
+
+  const handleDelete = (e) => {
+    siteInformation.handleDel
+  }
 
   return (
 
@@ -11,6 +22,7 @@ function TodoList (props) {
       {props.list.map(item => 
         <ListGroupItem as="li" className={`complete-${item.complete.toString()}`} key={item._id} onClick={() => props.handleComplete(item._id)}>{item.text}
           <span onClick={() => props.handleDelete(item._id)}>{`DELETE`}</span>
+          <span onClick={() => hideComplete(item._id)}>Hide from view</span>
         </ListGroupItem>
       )}
     </ListGroup>
